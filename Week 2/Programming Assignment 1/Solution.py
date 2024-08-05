3cafa74431d79aba809c9e0e9ed9cac036cac936
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-08-05 21:59:47 +0530

    Add the solution of Week 2 Programming Assignments of The Joy of Computing using Python Programming

---
+++ Week 2/Programming Assignment 1/Solution.py
@@ -0,0 +1,6 @@
+# write your code here
+n = int(input())
+if n < 0 :
+  print(-n , end="")
+else:
+  print(n , end="")
---
+++ Week 2/Programming Assignment 2/Solution.py
@@ -0,0 +1,3 @@
+# Write your code here
+n = int(input())
+print( a + (n-1)*d , end="")
